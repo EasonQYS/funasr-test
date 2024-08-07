2e5afa2b7e155f75f43bf1a7ad38fc5acca2c028
EasonQYS <easonqys@foxmail.com>
2024-08-07 16:44:09 +0800

    Update app.py

--- app.py
+++ app.py
@@ -8,11 +8,11 @@ print(os.system('ls -la /home/xlab-app-center/.cache/'))
 print(os.system('ls -la /home/xlab-app-center/.cache/modelscope'))
 print(os.system('ls -la /home/.cache/'))
 
-local_path = '/home/xlab-app-center/SenseVoiceSmall'
+local_path = '/home/xlab-app-center/.cache/modelscope/hub/iic/SenseVoiceSmall'
 if os.path.exists(local_path):
     print("文件已存在:", local_path)
 else:
-    model_dir = snapshot_download('iic/SenseVoiceSmall',local_dir=local_path)
+    model_dir = snapshot_download('iic/SenseVoiceSmall')
 print('pre model')
 model = AutoModel(
     model=local_path,
@@ -24,7 +24,7 @@ print('after model')
 
 def do(a):
     #print(a.name)
-    path = "./" + os.path.basename(a.name)  #NB*
+    path = "./" + os.path.basename(a.name)  # 复制到新目录
     shutil.copyfile(a.name, path)
     res = model.generate(
         input=path,
@@ -39,11 +39,11 @@ def do(a):
     
 def do2(a):
     print(a)
-    path = "./" + os.path.basename(a.name)  #NB*
+    path = "./" + os.path.basename(a.name)  # 复制到新目录
     shutil.copyfile(a.name, path)
     conn = sqlite3.connect(path)
     cursor = conn.cursor()
-    filename = 'data.json'
+    filename = '/home/xlab-app-center/data.json'
     # 执行SQL查询
     query = "SELECT * FROM 'PoketMessage' "
     cursor.execute(query)
@@ -79,7 +79,7 @@ def do2(a):
 
     # 关闭连接
     conn.close()
-    return '/home/xlab-app-center/data.json'
+    return filename
     
 with gr.Blocks() as demo:
     with gr.Row():
@@ -87,9 +87,9 @@ with gr.Blocks() as demo:
         t = gr.Textbox(label='转换文本的结果')
     b = gr.Button("立即转换")
     with gr.Row():
-        f1 = gr.File(file_types=['.db'])
-        b2 = gr.Button("语音转文本")
-        f2 = gr.File()
+        f1 = gr.File(file_types=['.db'],label='数据库聊天记录')
+        b2 = gr.Button("数据库转json")
+        f2 = gr.File(label='数据语料下载')
     file_path = gr.FileExplorer(root_dir='./')
     b.click(do, inputs=a,outputs=t)
     b2.click(do2, inputs=f1,outputs=f2)
